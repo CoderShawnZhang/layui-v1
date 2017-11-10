6c07ec71ea90de25bb26a6cc8319f5a707753c25
张洪波 <zhanghongbo@anlewo.com>
2017-11-10 18:16:10 +0800

    加入小部件注释

--- backend/controllers/WidgetsController.php
+++ backend/controllers/WidgetsController.php
@@ -21,25 +21,49 @@ use backend\models\Widget;
 
 class WidgetsController extends BaseController
 {
-    const WIDGET_STATUS_DISABLE = 0;//禁用
-    const WIDGET_STATUS_NORMAL = 1;//启用
-    const WIDGET_STATUS_UNINSTALL = 2;//卸载
-
+    /**
+     * 禁用
+     */
+    const WIDGET_STATUS_DISABLE = 0;
     const WIDGET_STATUS_TXT_DISABLE = '禁用';
+    /**
+     * 启用
+     */
+    const WIDGET_STATUS_NORMAL = 1;
     const WIDGET_STATUS_TXT_NORMAL = '启用';
+    /**
+     * 卸载
+     */
+    const WIDGET_STATUS_UNINSTALL = 2;//卸载
     const WIDGET_STATUS_TXT_UNINSTALL = '卸载';
 
+    /**
+     * 组合小部件常量数组
+     *
+     * @var array
+     */
     public static $widgetStatus =[
         self::WIDGET_STATUS_DISABLE=>['txt'=>self::WIDGET_STATUS_TXT_DISABLE,'color'=>'green'],
         self::WIDGET_STATUS_NORMAL=>['txt'=>self::WIDGET_STATUS_TXT_NORMAL,'color'=>'green'],
         self::WIDGET_STATUS_UNINSTALL=>['txt'=>self::WIDGET_STATUS_TXT_UNINSTALL,'color'=>'green'],
         ];
 
+    /**
+     * 渲染小部件页面
+     *
+     *
+     * @return string 渲染小部件view
+     */
     public function actionList(){
-
         return $this->render('list');
     }
 
+    /**
+     * 获取小部件列表
+     *
+     *
+     * @return string 返回layui-table 规定使用格式
+     */
     public function actionWidgetList()
     {
         $widget = Widget::find()->asArray()->all();
@@ -48,6 +72,15 @@ class WidgetsController extends BaseController
         }
         return $this->tableDataHeader($widget);
     }
+
+    /**
+     * 返回小部件中文状态文字显示
+     * （将需要写死数据常量化）
+     *
+     * @param $status 小部件状态
+     *
+     * @return string 中文状态
+     */
     private function WidgetStatus($status){
         if(isset(self::$widgetStatus[$status]['color'])){
             return '<span style="color:'.self::$widgetStatus[$status]['color'].'">'.self::$widgetStatus[$status]['txt'].'</span>';
@@ -55,6 +88,12 @@ class WidgetsController extends BaseController
         return self::$widgetStatus[$status]['txt'];
     }
 
+    /**
+     * 展示小部件功能
+     *
+     *
+     * @return string 渲染小部件展示页
+     */
     public function actionWidgetShow(){
         $widgetID = \Yii::$app->request->get('widgetID');
         $widget = Widget::find()->where(['id'=>$widgetID])->asArray()->one();
