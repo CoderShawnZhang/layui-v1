4bffab247bd7a677f18d14cc9e2c8fe103e0253a
张洪波 <zhanghongbo@anlewo.com>
2017-11-13 18:03:01 +0800

    切换控制上传图片【多图&单独】

--- backend/views/GetingData/UploadIndex.php
+++ backend/views/GetingData/UploadIndex.php
@@ -16,18 +16,14 @@
 </div>
 
 <div class="layui-upload">
-    <button type="button" class="layui-btn" id="test1">上传图片</button>
-    <div class="layui-upload-list">
-        <img class="layui-upload-img" id="demo1">
-        <p id="demoText"></p>
-    </div>
-</div>
-<fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
-    <legend>上传多张图片</legend>
-</fieldset>
-
-<div class="layui-upload">
-    <button type="button" class="layui-btn" id="test2">多图片上传</button>
+    <form class="layui-form" action="">
+        <div class="layui-input-inline" style="width: 100px;">
+            <button type="button" class="layui-btn" id="test1">上传图片</button>
+        </div>
+        <div class="layui-input-inline">
+            <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="多|单">
+        </div>
+    </form>
     <blockquote class="layui-elem-quote layui-quote-nm" style="margin-top: 10px;">
         预览图：
         <div class="layui-upload-list" id="demo2"></div>
@@ -45,27 +41,45 @@ use yii\widgets\ActiveForm;
 <?php ActiveForm::end() ?>
 
 <script>
-
-    layui.use(['upload','table','jquery'], function() {
+    var imagesCheck= true;
+    layui.use(['upload','table','jquery','form'], function() {
         var $ = layui.jquery
             ,table = layui.table
             ,$ = layui.jquery
+            ,form = layui.form
             ,upload = layui.upload;
         $("#test4").on('click',function(){
             $("#uploadimages").trigger('click');
+        });
+        //监听指定开关
+        form.on('switch(switchTest)', function(data){
+//            layer.msg('开关checked：'+ (this.checked ? 'true' : 'false'), {
+//                offset: '6px'
+//            });
+            if(this.checked){
+                imagesCheck = true;
+                document.getElementById("imageFile").multiple=true;
+                layer.tips('温馨提示：您开启了允许多图上传！', data.othis)
+            }else{
+                imagesCheck = false;
+                document.getElementById("imageFile").multiple=false;
+                layer.tips('温馨提示：您关闭了多图上传，只允许上传单张图片！', data.othis)
+            }
+            uploadInst.upload();
         });
         //普通图片上传
         var uploadInst = upload.render({
               elem: '#test1'
             , id:'imageFile'
-            , multiple: true  //指定多张图片
+            , multiple: imagesCheck  //指定多张图片
             , field:'UploadForm[imageFile]'
             , data:{file:$("input[name='UploadForm[imageFile]']")}
             , url: '/getingdata/upload'
             , before: function (obj) {
                 //预读本地文件示例，不支持ie8
                 obj.preview(function (index, file, result) {
-                    $('#demo1').attr('src', result); //图片链接（base64）
+                    $('#demo2').append('<img src="'+ result +'" alt="'+ file.name +'" class="layui-upload-img">')
+//                    $('#demo1').attr('src', result); //图片链接（base64）
                 });
             }
             , done: function (res) {
