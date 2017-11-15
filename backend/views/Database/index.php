1ac6d8a55267fe7142f80c172e98559ac50061ef
张洪波 <zhanghongbo@anlewo.com>
2017-11-15 18:09:38 +0800

    加入，弹框备份进度条

--- backend/views/Database/index.php
+++ backend/views/Database/index.php
@@ -31,9 +31,10 @@
 </script>
 
 <script>
-layui.use(['table','layer'],function(){
+layui.use(['table','layer','element'],function(){
         var table = layui.table,
             $ = layui.jquery,
+            element = layui.element,
             layer = layui.layer;
 
         table.render({
@@ -70,6 +71,31 @@ layui.use(['table','layer'],function(){
                 var type = othis.data('type')
                     ,action = othis.data('method');
 
+                    //示范一个公告层
+                    layer.open({
+                        type: 1
+                        ,title: false //不显示标题栏
+                        ,closeBtn: false
+                        ,area: '300px;'
+                        ,shade: 0.8
+                        ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
+                        ,btn: []
+                        ,btnAlign: 'c'
+                        ,moveType: 1 //拖拽模式，0或者1
+                        ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">' +
+                            '<div class="layui-progress layui-progress-big" lay-showpercent="true" lay-filter="demo">' +
+                            '<div class="layui-progress-bar layui-bg-red" lay-percent="0%"></div>' +
+                            '</div></div>'
+                        ,success: function(layero){
+                            var btn = layero.find('.layui-layer-btn');
+                            btn.find('.layui-layer-btn0').attr({
+                                href: 'http://www.layui.com/'
+                                ,target: '_blank'
+                            });
+                        }
+                    });
+                active.loading(othis);
+                    return false;
                 $.get('/database/'+action,function(data){
                     layer.open({
                         type: 1
@@ -102,6 +128,26 @@ layui.use(['table','layer'],function(){
                         }
                     });
 
+            },
+            loading: function(othis){
+                alert(12312312);
+                var DISABLED = 'layui-btn-disabled';
+                if(othis.hasClass(DISABLED)) return;
+                //模拟loading
+                var n = 0, timer = setInterval(function(){
+                    n = n + Math.random()*10|0;
+                    if(n>100){
+                        n = 100;
+                        clearInterval(timer);
+                        othis.removeClass(DISABLED);
+                    }
+                    element.progress('demo', n+'%');
+                    if(n==100){
+                        alert("备份完成！");
+                    }
+                }, 300+Math.random()*1000);
+
+                othis.addClass(DISABLED);
             }
         };
         $('#layerDemo .layui-btn').on('click', function(){
